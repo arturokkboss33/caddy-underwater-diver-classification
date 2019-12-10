fb24ea22007d59c63c546dce0f2ecc59f870f725
Andrea Ranieri <andrea.ranieri@cnr.it>
2019-12-10 14:00:25 +0100

    Added numbers (=command codes) to caddian_gen.py to match the ones in the caddian-syntax-checker.

--- underwater-vision-based-gesture-recognition-notebooks-related-files/caddian-generator/caddian_gen.py
+++ underwater-vision-based-gesture-recognition-notebooks-related-files/caddian-generator/caddian_gen.py
@@ -20,6 +20,7 @@ from random import randrange
 def create_mosaic_num(sentence, num_1, num_2):
 	#print('pre-mosaic sentence: ' + sentence)
 	sentence += "mosaic " + str(num_1) + " num_delimiter " + str(num_2) + " num_delimiter "
+	sentence += "20," + str(num_1) + ",12," + str(num_2) + ",12,"
 	return sentence
 
 ##################################################
@@ -34,9 +35,11 @@ def gen_sentence(idx, seed, list_of_commands):
 	#print("--Creating sentence of %d length commands" % num_commands)
 	num_commands = randrange(1,3)
 	sentence=''
+	sentence_numbers=''
 	print("Creating sentence %d composed by %d commands" % (idx, num_commands))
 	for i in range(num_commands):
 		sentence += "start_comm "
+		sentence_numbers+="10,"
 		# choose a random command
 		index = randrange(0,7)
 		chosen = commands[index]
@@ -48,20 +51,29 @@ def gen_sentence(idx, seed, list_of_commands):
 			#print('post-mosaic sentence: ' + sentence)
 		elif chosen == "up":
 			sentence += "up " + str(randrange(1,4)) + " num_delimiter "
+			sentence_numbers += "21," + str(randrange(1,4)) + ",12,"
 		elif chosen == "down":
 			sentence += "down " + str(randrange(1,4)) + " num_delimiter "
+			sentence_numbers += "22," + str(randrange(1,4)) + ",12,"
 		elif chosen == "backwards":
 			sentence += "backwards " + str(randrange(1,4)) + " num_delimiter "
+			sentence_numbers += "24," + str(randrange(1,4)) + ",12,"
 		elif chosen == "boat":
 			sentence += "boat "
+			sentence_numbers += "28,"
 		elif chosen == "carry":
 			sentence += "carry "
+			sentence_numbers += "26,"
 		elif chosen == "photo":
 			sentence += "photo "
+			sentence_numbers += "25,"
 		elif chosen == "here":
 			sentence += "here "
+			sentence_numbers += "27,"
 	sentence += "end_comm"
+	sentence_numbers += "11"
 	print(sentence)
+	print(sentence_numbers)
 
 ##################################################
 def main(argv):
